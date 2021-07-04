8037d8cd3b89645a355ad673d08f4757cd4329ea
luckmerlin <676046361>
2021-07-04 17:47:27 +0800

    Add widget module

---
+++ Widgets/src/main/java/luckmerlin/widget/layout/FlowLayout.java
@@ -0,0 +1,44 @@
+package luckmerlin.widget.layout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FlowLayout extends ViewGroup {
+
+    public FlowLayout(Context context) {
+        this(context,null);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @SuppressLint("NewApi")
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count=getChildCount();
+        View child=null;
+        for (int i = 0; i < count; i++) {
+            if (null==(child=getChildAt(i))||child.getVisibility()==View.GONE){
+                continue;
+            }
+            measureChild(child,widthMeasureSpec,heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
